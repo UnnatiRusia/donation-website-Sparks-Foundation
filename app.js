7f0062813d97ee920a4099a9da1e8a03f8f6f9db
UnnatiRusia <unnatirusia154@gmail.com>
2021-03-14 16:07:06 +0530

    final

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.get("/index",function(req,res) {
+app.get("/",function(req,res) {
    res.render("index")
 })
 app.get("/contact",function(req,res) {
